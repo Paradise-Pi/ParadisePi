f82ba70e05094d3ae7290b624bffcef7588a6bbb
James Bithell <james@bithell.com>
2021-02-11 13:49:04 +0000

    Remove security requirement of IPC commands (just bridge everything), and add async function support with callbacks

--- preload.js
+++ preload.js
@@ -2,37 +2,19 @@ const {
   contextBridge,
   ipcRenderer
 } = require("electron");
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
-
-  for (const type of ['chrome', 'node', 'electron','e131']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
-})
-
 
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld(
     "api", {
       send: (channel, data) => {
-        // whitelist channels
-        let validChannels = ["sendACN", "sendOSC", "queryDB"];
-        if (validChannels.includes(channel)) {
-          ipcRenderer.send(channel, data);
-        }
+        ipcRenderer.send(channel, data);
       },
       receive: (channel, func) => {
-        let validChannels = ["fromOSC", "dbRequestReply"];
-        if (validChannels.includes(channel)) {
-          // Deliberately strip event as it includes `sender`
-          ipcRenderer.on(channel, (event, ...args) => func(...args));
+        ipcRenderer.on(channel, (event, ...args) => func(...args));
+      },
+        asyncSend: async (channel, data) => {
+            return await ipcRenderer.invoke(channel, data);
         }
-      }
     }
 );
