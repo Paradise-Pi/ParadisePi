7a3c9720b34591c9d80e2a2b064edd564a11b795
James Bithell <Jbithell@users.noreply.github.com>
2022-04-18 11:50:23 +0100

    Add eslint and run it

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+  },
+  'rules': {
+    'require-jsdoc': ['off'],
+    'valid-jsdoc': ['off'],
+    'max-len': ['off'],
+  },
+  'ignorePatterns': ['node_modules/', '.vscode/', 'assets/'],
+};
