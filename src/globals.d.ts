08f12a152c740386eaa6b9a0b1336ad34ea2493c
James Bithell <Jbithell@users.noreply.github.com>
2022-04-23 16:21:31 +0100

    Add e131 to global scope

---
+++ src/globals.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-var */
+/**
+ * https://github.com/Microsoft/TypeScript/pull/29332
+ */
+
+import E131 from "./output/e131";
+
+declare global {
+  var e131: E131;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { app, BrowserWindow } from 'electron';
 import "reflect-metadata";
 import createMainWindow from './createMainWindow';
+import dataSource from './database/dataSource';
+import fs from 'fs';
+import { AdminServer } from './admin-server';
+import { LxConfigRepository } from './database/repositories/config';
+import E131 from './output/e131';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -13,7 +18,39 @@ if (require('electron-squirrel-startup')) {
 // Some APIs can only be used after this event occurs.
 
 app.whenReady().then(() => {
-  createMainWindow();
+  // Populate the about info
+  const versions = JSON.stringify(process.versions);
+  app.setAboutPanelOptions({
+    applicationName: 'Paradise',
+    applicationVersion: app.getVersion(),
+    version: app.getVersion(),
+    credits: versions,
+    copyright: '©2021-22 James Bithell & John Cherry',
+  });
+  // Backup the database on boot
+  fs.copyFile('database.sqlite', 'admin/database.sqlite', (err) => {
+    if (!err) {
+      console.log('Database was backed up');
+    }
+    dataSource.initialize().then(() => {
+      if (process.argv.includes('--e131sampler')) {
+        //setupE131Sampler();
+        //createWindow('e131sampler.html');
+      } else {
+        //setupOSC();
+        if (LxConfigRepository.getItem("e131Enabled")) {
+          globalThis.e131 = new E131(); // Have a single version of the class because it locks the network output
+        }
+        createMainWindow();
+        const adminServer = new AdminServer();
+      }
+      
+    }).catch((err) => {
+      // TODO handle with a popup to user
+      console.error("Error during Data Source initialization", err)
+    })
+  });
+  
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
   });
