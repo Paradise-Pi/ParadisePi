7a01f59a96614f6b48a9427d94a1b319022a909f
James Bithell <Jbithell@users.noreply.github.com>
2022-05-12 17:58:23 +0100

    Add QR Code package

--- src/app/Pages/About.tsx
+++ src/app/Pages/About.tsx
@@ -1,4 +1,5 @@
 import { Grid, Paper, SimpleGrid, Text } from "@mantine/core";
+import { QRCodeSVG } from "qrcode.react";
 import React from "react";
 
 export const AboutPage = () => {
@@ -10,6 +11,7 @@ export const AboutPage = () => {
           Facility control panel for sACN & OSC, in Electron.
         </Text>
       </Paper>
+      <QRCodeSVG value="https://google.com/" bgColor="#000000" fgColor="#FFFFFF" />
     </>
     );
 }
