527e5f10fc5f05eb2b9708fb357ca05e7b66e13e
John Cherry <jac677@york.ac.uk>
2022-05-21 14:48:36 +0100

    Add E131 fading (#21)
    * Add basic e131 fade function
    
    * Change function to allow multiple fades at once

--- src/output/e131/index.ts
+++ src/output/e131/index.ts
@@ -2,15 +2,31 @@ import { networkInterfaces } from 'os'
 import e131Lib from '@paradise-pi/e131'
 import { ConfigRepository } from '../../database/repository/config'
 
+interface channelData {
+	channel: number
+	level: number
+}
+
+interface channelFade {
+	channel: number
+	universe: number
+	fadeFrom: number
+	fadeTo: number
+	fadeFromTimestamp: number
+	fadeToTimestamp: number
+}
+
 export default class E131 {
 	// sACN = E131
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	public e131Clients: Array<any> // Bit cheeky, but disabling the check because there are no typings for the lib
+	private fades: Array<channelFade>
 	private firstUniverse: number
 	private universes: number
 	private sourceName: string
 	private priority: number
 	private frequency: number
+
 	constructor() {
 		this.firstUniverse = parseInt(
 			ConfigRepository.getItem('e131FirstUniverse')
@@ -28,6 +44,7 @@ export default class E131 {
 	}
 	setupUniverses() {
 		this.e131Clients = []
+		this.fades = []
 		for (
 			let i = this.firstUniverse;
 			i <= this.firstUniverse + this.universes - 1;
@@ -56,14 +73,88 @@ export default class E131 {
 			}
 		}
 	}
+	//Keep e131 alive by regularly transmitting state.
 	send(universe: number) {
+		// eslint-disable-next-line @typescript-eslint/no-this-alias
+		const self = this
+		//every cycle, check whether anything is fading
+		this.checkForFades()
 		this.e131Clients[universe]['client'].send(
 			this.e131Clients[universe]['packet'],
 			function () {
 				setTimeout(function () {
-					this.send(universe)
-				}, 1000 / this.frequency)
+					self.send(universe)
+				}, 1000 / self.frequency)
 			}
 		)
+	}
+
+	checkForFades() {
+		const timeNow = +new Date()
+		//fades is the array of current fades
+		for (const channel of this.fades) {
+			if (channel.fadeToTimestamp >= timeNow) {
+				//how far through time period are we
+				let fadePercent = 1
+				if (channel.fadeFromTimestamp != channel.fadeToTimestamp) {
+					fadePercent =
+						(timeNow - channel.fadeFromTimestamp) /
+						(channel.fadeToTimestamp - channel.fadeFromTimestamp)
+				}
+				//update channel to that level
+				this.e131Clients[channel.universe]['addressData'][
+					channel.channel - 1
+				] =
+					channel.fadeFrom +
+					(channel.fadeTo - channel.fadeFrom) * fadePercent
+
+				//time is finished so remove from array
+			} else {
+				this.fades.splice(
+					this.fades.findIndex(
+						item => item.channel === channel.channel
+					),
+					1
+				)
+			}
+		}
+	}
+
+	/**
+	 * Update a given universe's channel levels
+	 * @param universe universe number between 1 and 63999
+	 * @param channelData  - [channel:number, level:number]
+	 * @param fadeTime (optional) Fade time in ms
+	 */
+	update(
+		thisUniverse: number,
+		channelData: Array<channelData>,
+		fadeTime = 0
+	) {
+		const dateNow = new Date()
+		for (const thisChannel of channelData) {
+			const thisFade = {
+				channel: thisChannel.channel,
+				universe: thisUniverse,
+				fadeFrom:
+					// eslint-disable-next-line prettier/prettier
+					this.e131Clients[thisUniverse]['addressData'][thisChannel.channel - 1],
+				fadeTo: thisChannel.level,
+				fadeFromTimestamp: +dateNow,
+				fadeToTimestamp: +new Date(dateNow.getTime() + fadeTime) + 1, //fadetime + 1 allows final value to be set
+			}
+
+			//remove existing fade from array
+			const currentIndex = this.fades.findIndex(
+				item => item.channel == thisChannel.channel
+			)
+			//if >-1, channel already exists in array
+			if (currentIndex > -1) {
+				//remove the old fade
+				this.fades.splice(currentIndex, 1)
+			}
+			//always add new fade
+			this.fades.push(thisFade)
+		}
 	}
 }
