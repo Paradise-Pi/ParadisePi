495688f6f2060ca52b7585342f128ac33a356258
James Bithell <Jbithell@users.noreply.github.com>
2022-06-12 09:42:44 +0100

    Catch being unable to test ports

--- src/webServer/availablePorts.ts
+++ src/webServer/availablePorts.ts
@@ -5,30 +5,43 @@ import portastic from 'portastic'
  */
 export const getAvailablePort = (): Promise<number> => {
 	const desiredPort = process.env.WEB_SERVER_PORT ? parseInt(process.env.WEB_SERVER_PORT) : 80
+	return new Promise(resolve => {
+		return portastic
+			.test(desiredPort)
+			.then(isOpen => {
+				if (isOpen) resolve(desiredPort)
+				else return trySimilarPorts(desiredPort)
+			})
+			.catch(() => {
+				return trySimilarPorts(desiredPort)
+			})
+	})
+}
+const trySimilarPorts = (desiredPort: number): Promise<number> => {
+	return new Promise(resolve => {
+		return portastic
+			.find({ min: desiredPort, max: desiredPort + 20, retrieve: 1 })
+			.then(ports => {
+				if (ports.length > 0) resolve(ports[0])
+				else {
+					return tryPortsAbove8080()
+				}
+			})
+			.catch(() => {
+				return tryPortsAbove8080
+			})
+	})
+}
+const tryPortsAbove8080 = (): Promise<number> => {
 	return new Promise((resolve, reject) => {
-		return portastic.test(desiredPort).then(function (isOpen) {
-			if (isOpen) resolve(desiredPort)
-			else {
-				return portastic
-					.find({ min: desiredPort, max: desiredPort + 20, retrieve: 1 })
-					.then(function (ports) {
-						if (ports.length > 0) resolve(ports[0])
-						else {
-							return portastic
-								.find({ min: 8080, max: 8999, retrieve: 1 })
-								.then(function (ports) {
-									if (ports.length > 0) resolve(ports[0])
-									else reject('No ports available')
-								})
-								.catch(function (err) {
-									reject(err)
-								})
-						}
-					})
-					.catch(function (err) {
-						reject(err)
-					})
-			}
-		})
+		return portastic
+			.find({ min: 8080, max: 8999, retrieve: 1 })
+			.then(ports => {
+				if (ports.length > 0) resolve(ports[0])
+				else reject('No ports available')
+			})
+			.catch(function (err) {
+				reject(err)
+			})
 	})
 }
