bfff42012feabbd99a0aed02227f4ff4fb9f8202
James Bithell <Jbithell@users.noreply.github.com>
2022-06-19 12:37:37 +0100

    Improve new db upload UI

--- src/app/Pages/Admin/ModuleConfiguration/Database.tsx
+++ src/app/Pages/Admin/ModuleConfiguration/Database.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import { Blockquote, Button, Text } from '@mantine/core'
+import React, { useState } from 'react'
+import { Alert, Blockquote, Button, Modal, Text } from '@mantine/core'
 import { runningInElectron } from '../../../apis/utilities/version'
+import { FaExclamationTriangle } from '@react-icons/all-files/fa/FaExclamationTriangle'
 
 export const DatabaseConfigurationPage = () => {
+	const [showModal, setShowModal] = useState(false)
 	if (runningInElectron())
 		return <Blockquote icon={null}>To backup or reset the database, please use the web interface.</Blockquote>
 	return (
@@ -12,16 +14,23 @@ export const DatabaseConfigurationPage = () => {
 					Download Database Backup
 				</Button>
 			</a>
-			<p>
-				Caution! Ensure you are uploading a valid backup file from Paradise. This file will not be checked
-				before being set as the device config!
-			</p>
-			<form action="/database/upload" method="post" encType="multipart/form-data">
-				<input className="form-control" type="file" name="file" accept=".sqlite,.sqlite3" />
-				<button className="btn btn-sm btn-success" style={{ margin: '10px' }} type="submit">
-					Upload
-				</button>
-			</form>
+			<Button variant="default" color="dark" size="md" mx="xs" my="xs" onClick={() => setShowModal(true)}>
+				Upload new Database
+			</Button>
+			<Modal onClose={() => setShowModal(false)} opened={showModal} title="Upload new Database">
+				<Alert icon={<FaExclamationTriangle />} title="Danger" color="gray" my="sm">
+					Ensure you are uploading a valid backup file from Paradise as this file will not be checked to
+					ensure it is a valid backup file - if this is not a backup file, you will lose all data and will
+					have to uninstall Paradise and install it again. Please also ensure no other users are currently
+					using the system, as all lighting and sound will be lost.
+				</Alert>
+				<form action="/database/upload" method="post" encType="multipart/form-data">
+					<input className="form-control" type="file" name="file" accept=".sqlite,.sqlite3" />
+					<Button type="submit" color="red">
+						Upload
+					</Button>
+				</form>
+			</Modal>
 		</>
 	)
 }
