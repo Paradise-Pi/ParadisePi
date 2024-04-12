2d997501ae43ff6dbb4f0fb1fb0ad1252875950c
James Bithell <Jbithell@users.noreply.github.com>
2024-04-12 13:52:46 +0100

    Some limited progress...

--- forge.config.ts
+++ forge.config.ts
@@ -1,16 +1,20 @@
-import type { ForgeConfig } from '@electron-forge/shared-types'
+import { MakerDeb } from '@electron-forge/maker-deb'
+import MakerDMG from '@electron-forge/maker-dmg'
 import { MakerSquirrel } from '@electron-forge/maker-squirrel'
 import { MakerZIP } from '@electron-forge/maker-zip'
-import { MakerDeb } from '@electron-forge/maker-deb'
 import { WebpackPlugin } from '@electron-forge/plugin-webpack'
-
+import PublisherGithub from '@electron-forge/publisher-github'
+import type { ForgeConfig } from '@electron-forge/shared-types'
 import { mainConfig } from './webpack.main.config'
 import { rendererConfig } from './webpack.renderer.config'
-import MakerDMG from '@electron-forge/maker-dmg'
-import PublisherGithub from '@electron-forge/publisher-github'
-
 const config: ForgeConfig = {
-	packagerConfig: {},
+	packagerConfig: {
+		afterComplete: [
+			async (buildPath, electronVersion, platform, arch, done) => {
+				throw new Error(`${__dirname}***${buildPath}***${electronVersion}***${platform}***${arch}***`)
+			},
+		],
+	},
 	rebuildConfig: {},
 	makers: [
 		new MakerSquirrel({}),
