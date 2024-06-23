0a3106df0018235f8c6e5ce11d00abe36e6e4285
James Bithell <Jbithell@users.noreply.github.com>
2024-06-23 17:49:28 +0100

    Update broadcast.ts

--- server/src/api/broadcast.ts
+++ server/src/api/broadcast.ts
@@ -1,6 +1,5 @@
-//import { WebServer } from '../webServer'
-// TODO re-enable this
+import { WebServer } from '../webServer'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const broadcast = (channel: string, data: { [key: string]: any }): void => {
-	//WebServer.socketIo.emit(channel, data)
+	WebServer.socketIo.emit(channel, data)
 }
