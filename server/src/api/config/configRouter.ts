a3ddf75dbb55f3f188934637e3de21ad6c9a1562
James Bithell <Jbithell@users.noreply.github.com>
2024-07-06 18:43:55 +0100

    Be more sensitive to restarting whole app

--- server/src/api/config/configRouter.ts
+++ server/src/api/config/configRouter.ts
@@ -23,39 +23,41 @@ export const configRouter = (
 		if (method === 'POST') {
 			let restartE131 = false
 			let restartWholeApp = false
-			return Promise.all(
-				Object.entries(payload).map(async ([key, value]) => {
-					if (value === true) value = 'true'
-					else if (value === false) value = 'false'
-					else if (value === 'null') value = null
-					if (key.includes('e131')) restartE131 = true // Only restart e131 output IF there is some e131 that's been changed
-					if (key === 'historyEnabled') restartWholeApp = true // Restart the whole app if history mode is enabled/disabled as it takes effect at reboot time
-					if (key === 'fullscreen') restartWholeApp = true // Restart the whole app if fullscreen mode is enabled/disabled as it takes effect at reboot time
-					await ConfigRepository.save({
-						key,
-						value,
+			return ConfigRepository.getItem('fullscreen').then(fullscreen =>
+				Promise.all(
+					Object.entries(payload).map(async ([key, value]) => {
+						if (value === true) value = 'true'
+						else if (value === false) value = 'false'
+						else if (value === 'null') value = null
+						if (key.includes('e131')) restartE131 = true // Only restart e131 output IF there is some e131 that's been changed
+						if (key === 'historyEnabled') restartWholeApp = true // Restart the whole app if history mode is enabled/disabled as it takes effect at reboot time
+						if (key === 'fullscreen' && fullscreen !== value) restartWholeApp = true // Restart the whole app if fullscreen mode is enabled/disabled as it takes effect at reboot time
+						await ConfigRepository.save({
+							key,
+							value,
+						})
 					})
-				})
+				)
+					.then(() => {
+						return createDatabaseObject('change of config')
+					})
+					.then((response: Database) => {
+						sendDatabaseObject(response)
+						if (restartE131) return destroyE131()
+						return Promise.resolve()
+					})
+					.then(() => {
+						if (restartWholeApp) reboot(true)
+						else {
+							if (restartE131) createE131()
+							// Recreate OSC connection
+							destroyOSC()
+							createOSC()
+						}
+						// Return response to window
+						resolve({})
+					})
 			)
-				.then(() => {
-					return createDatabaseObject('change of config')
-				})
-				.then((response: Database) => {
-					sendDatabaseObject(response)
-					if (restartE131) return destroyE131()
-					return Promise.resolve()
-				})
-				.then(() => {
-					if (restartWholeApp) reboot(true)
-					else {
-						if (restartE131) createE131()
-						// Recreate OSC connection
-						destroyOSC()
-						createOSC()
-					}
-					// Return response to window
-					resolve({})
-				})
 		} else reject(new Error('Path not found'))
 	})
 }
