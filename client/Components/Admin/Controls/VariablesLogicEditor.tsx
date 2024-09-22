cbc8b2ef785c8b76beb9ee42d9e317ebe186d441
James Bithell <Jbithell@users.noreply.github.com>
2024-09-22 12:33:01 +0100

    Update VariablesLogicEditor.tsx

--- client/Components/Admin/Controls/VariablesLogicEditor.tsx
+++ client/Components/Admin/Controls/VariablesLogicEditor.tsx
@@ -86,7 +86,7 @@ export const VariablesLogicEditor = (props: InputProps) => {
 					{['isnull', 'isnotnull'].includes(form.values.rules[index].logic) ? (
 						<input type="hidden" {...form.getInputProps(`rules.${index}.match`)} />
 					) : (
-						<TextInput {...form.getInputProps(`rules.${index}.match`)} placeholder="to" />
+						<TextInput {...form.getInputProps(`rules.${index}.match`)} placeholder="this string" />
 					)}
 					<Select
 						placeholder="Then set variable"
